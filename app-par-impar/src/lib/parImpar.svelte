adc145cdf7c1777a40ad31cd820a2cd5d33c390c
ElMataBots <mariojosemonrroyduran@gmail.com>
2024-04-24 11:31:29 -0500

    first commit

---
+++ app-par-impar/src/lib/parImpar.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let numero;
+    let resultado;
+    $: if (numero===0){
+        resultado="neutro";
+    }
+        else {
+            if (numero%2===0)
+                resultado="par";
+            else
+                resultado="impar";
+        }
+</script>
+<p>El número {numero} es {resultado} </p>
+<style>
+    p{
+        color:rgb(255, 255, 255);
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 25px;
+    }
+</style>
